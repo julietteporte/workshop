bff202676bec08dac52ed9a730929e0a901ef481
Thomas Le Saux <toto3336@hotmail.fr>
2016-11-04 15:30:21 +0100

    Nouveau main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int addition (int a, int b)
+{
+    return a+b;
+}
+
+int soustraction (int a, int b)
+{
+    return a-b;
+}
+
+int main()
+{
+
+    printf("L'addition de a et b vaut : %d\n", addition(1,4));
+    printf("La soustraction de a et b vaut : %d\n", soustraction(1,4));
+    return 0;
+}
